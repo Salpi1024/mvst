dcd24156e87f855d0b2f0418de8b10e750da6561
Salvatore Pariota <salpariota@gmail.com>
2021-05-19 17:37:18 +0200

    feat: added linter and setted it up

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import './App.css';
-import {
-  useQuery,
-  gql
-} from '@apollo/client';
-import {useEffect} from 'react'
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
 
-const LOGIN = gql `
-query Query($login: String!) {
+const LOGIN = gql`
+  query Query($login: String!) {
     user(login: $login) {
-    bio
-    email
-  } 
+      bio
+      email
     }
-  
-`
+  }
+`;
 
 function App() {
-  const { loading, error, data } = useQuery(LOGIN, {variables: {login: "Salpi1024"} });
+  const { loading, error, data } = useQuery(LOGIN, { variables: { login: 'Salpi1024' } });
   useEffect(() => {
-   if(data) console.log(data);
-  
-  }, [data])
-  if (loading) return <h1>Loading...</h1> ;
+    if (data) console.log(data);  // eslint-disable-line 
+  }, [data]);
+  if (loading) return <h1>Loading...</h1>;
   if (error) return <h1> Error! ${error.message}</h1>;
   return (
-    
     <div className="App">
-     <h1>{data.user.email}</h1>  
+      <h1>{data.user.email}</h1>
     </div>
-
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: `${process.env.REACT_APP_GH_URI}`,
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_MY_ENV}`
+    Authorization: `Bearer ${process.env.REACT_APP_MY_ENV}`,
   },
- });
+});
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-    <App />
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
