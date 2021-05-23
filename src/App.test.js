66642cec30ebad1bfe46745e3a4205a400552aee
Salvatore Pariota <salpariota@gmail.com>
2021-05-23 17:51:18 +0200

    test: updated mockQuery to contain just fake data

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 import { MockedProvider } from '@apollo/client/testing';
-// import mockQuery from './mocks';
 import { mockQuery } from './mocks';
 
-it('renders the Login query', async () => {
+it('renders the LOGIN query', async () => {
   const { findByText } = render(
     <MockedProvider mocks={mockQuery} addTypename={false}>
       <App />
     </MockedProvider>
   );
-  const userName = await findByText('Awesomeness');
-  expect(userName).toBeInTheDocument();
-  
+  const mockUsername = await findByText('Awesomeness');
+  const firstMockRepoName = await findByText('another-amazing-app');
+  expect(mockUsername).toBeInTheDocument();
+  expect(firstMockRepoName).toBeInTheDocument();
 });
